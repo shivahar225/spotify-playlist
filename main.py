707af7e2d80f4905eafe9b4b3f998aa00ccf0f00
shivahar225 <168921705+shivahar225@users.noreply.github.com>
2024-12-17 10:26:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="",
+        client_secret= "",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+song_uris = ["The list of", "song URIs", "you got by", "searching Spotify"]
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+# print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]:
+
+print(song_names)
